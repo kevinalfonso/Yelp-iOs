85a49ec56a53f590f0bc0d8e53aa03c831c1131f
Kevin Alfonso <kevin@Kevins-MacBook-Pro.local>
2017-02-15 18:34:27 -0500

    commit

---
+++ Yelp/BusinessesViewController.swift
@@ -0,0 +1,123 @@
+//
+//  BusinessesViewController.swift
+//  Yelp
+//
+//  Created by Timothy Lee on 4/23/15.
+//  Copyright (c) 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var businesses: [Business]!
+    
+    var searchBar = UISearchBar()
+    
+    //tableView OUTLET
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        // for Scroll Height Dimension
+        tableView.estimatedRowHeight = 120
+        
+        //let searchBar = UISearchBar()
+        searchBar.sizeToFit()
+        
+        navigationItem.titleView = searchBar
+        
+        
+        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            // Updates The tableView after data is taken
+            self.tableView.reloadData()
+            if let businesses = businesses {
+                for business in businesses {
+                    print(business.name!)
+                    print(business.address!)
+                }
+            }
+            
+            }
+        )
+        
+        /* Example of Yelp search with more search options specified
+         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+         self.businesses = businesses
+         
+         for business in businesses {
+         print(business.name!)
+         print(business.address!)
+         }
+         }
+         */
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        cell.business = businesses[indexPath.row]
+        return cell
+        
+    }
+    
+    /*
+    // SEARCH BAR: TEXT DID CHANGE
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        businesses = searchText.isEmpty ? businesses : businesses?.filter({(data: Array) -> Bool in
+            let title = data["name"] as! String
+            return title.range(of: searchText, options: .caseInsensitive) != nil
+        })
+        
+        tableView.reloadData()
+    }
+
+    
+    
+    // SEARCH BAR TEXT
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    
+    // SEARCH BAR CANCEL
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+     */
+
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
